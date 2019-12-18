189008bb36512f3d120a40662634c4e563be39e0
SodaCode <dominicsaechao1@gmail.com>
2019-12-18 13:42:45 -0800

    Added a navbar and colored the hero section with a black bg and pink text. Debating on adding a thrid color to the theme. About html component is framed out.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { TourComponent } from './components/tour/tour.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, AboutComponent, TourComponent],
+  declarations: [AppComponent, HomeComponent, AboutComponent, TourComponent, NavbarComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
